b9b68c2c1cabbaecb7dda061a75478d047ac5b0f
Nikolay Vinogradov <NikolayVinogradov15@yandex.ru>
2021-10-10 18:04:10 +0300

    Add Producers and concumers

---
+++ Consumer/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            string message = string.Empty;
+
+            do
+            {
+                using (var connection = factory.CreateConnection())
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "dev-queue", autoDelete: false, durable: false, exclusive: false);
+                    var consumer = new EventingBasicConsumer(channel);
+
+                    Thread.Sleep(1000);
+
+                    consumer.Received += (sender, e) =>
+                    {
+                        var body = e.Body;
+                        message = Encoding.UTF8.GetString(body.ToArray());
+                        Console.WriteLine(message);
+                    };
+
+
+
+                    channel.BasicConsume("dev-queue", true, consumer);
+                }
+            } while (true);
+
+        }
+    }
+}
---
+++ RabbitMQ/Program.cs
@@ -0,0 +1,45 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace RabbitMQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+
+            int i = 0;
+            var random = new Random();
+
+            do
+            {
+                var timeout = random.Next(1000, 3000);
+                Thread.Sleep(timeout);
+
+                using (var connection = factory.CreateConnection())
+                {
+                    using (var channel = connection.CreateModel())
+                    {
+                        channel.QueueDeclare(queue: "dev-queue", autoDelete: false, durable: false, exclusive: false);
+                        string message = $"This is new message from publisher with duration {timeout}";
+
+                        var messageBody = Encoding.UTF8.GetBytes(message);
+
+                        channel.BasicPublish("", "dev-queue", null, messageBody);
+
+                        Console.WriteLine("Message is sent " + timeout);
+                    }
+
+                }
+
+                i++;
+            }
+
+            while (i < 50);
+
+        }
+    }
+}
